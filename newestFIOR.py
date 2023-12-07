35bc77b5cd4631fc1642ec2f2e09ca44975d40e9
Cassel Robson <casselrobsongithub@gmail.com>
2023-12-07 15:11:28 -0500

    Updated

--- newestFIOR.py
+++ newestFIOR.py
@@ -1,92 +1,97 @@
-from scipy.optimize import linprog
+from scipy.optimize import minimize
 import numpy as np
-from main import *
+from newmain import *
+
 def FIOR_LP(G, state, action, orderID, orderLog, originalorderAmount, starting_pool, S, totalcost):
     orderAmount = originalorderAmount
     visited = set()
     order_exhausted = [False]
-    starting_pools = list(G.keys())
-    starting_pools.sort(key=lambda pool: state["AskPrice"][pool]) 
-    # Linear Programming: Define the objective function coefficients (costs)
-    c = [state["AskPrice"][pool] for pool in action["AvailablePools"]]
-
+    previous_node = starting_pool
+    next_node = optimal_node_LP(G, state, starting_pool, previous_node)
+    
     while sum(state["Availability"].values()) > 0:
-        # Linear Programming: Define the inequality constraints (availability constraints)
-        A = [[-1 if pool == target else 0 for target in action["AvailablePools"]] for pool in action["AvailablePools"]]
-        b = [0] * len(action["AvailablePools"])
-
-        # Linear Programming: Solve the linear program
-        result = linprog(c, A_ub=A, b_ub=b)
-
-        # Get the next pool to execute the order
-        next_pool_index = np.argmin(result.x)
-        next_pool = action["AvailablePools"][next_pool_index]
-
         # Execute action
-        totalcost, min_pool, orderticket, orderAmount = transition(next_pool, visited, starting_pools, G, state, action, orderAmount, order_exhausted, S, totalcost, originalorderAmount)
-        print("Pool with the next most optimal ask price is:", min_pool)
-        print("Amount spent at pool ", min_pool, " is: ", orderticket)
-        print("Number of bonds purchased at ", min_pool, " is: ", originalorderAmount - orderAmount)
-        print("Number of bonds remaining in order is: ", orderAmount)
-        print("Current total cost of the order --> ", totalcost)
-        print()
+        totalcost, pool_traversed, orderticket, orderAmount = transition(next_node, visited, G, state, action, orderAmount, order_exhausted, S, totalcost, originalorderAmount)
+        previous_node = starting_pool
+        starting_pool = pool_traversed
         
+        next_node = optimal_node_LP(G, state, starting_pool, previous_node)
         
+    print("Pool with the next most optimal ask price is:", pool_traversed)
+    print("Amount spent at pool ", pool_traversed, " is: ", orderticket)
+    print("Number of bonds purchased at ", pool_traversed, " is: ", originalorderAmount - orderAmount)
+    print("Number of bonds remaining in order is: ", orderAmount)
+    print("Current total cost of the order --> ", totalcost)
+    print()
+
     return [visited], totalcost
 
+def optimal_node_LP(G, state, starting_node, previous_node):
+    # Extract nodes and ask prices
+    print("Previous Node: ", previous_node)
+    print("Currently at node: ", starting_node)
+    neighbors = G.get(starting_node, [])
+    print("All direct neighbors --> ", neighbors)
+    neighbors_with_availability = [node for node in neighbors if state["Availability"][node] > 0]
+    print("All direct neighbors with bonds available --> ", neighbors_with_availability)
+    ask_prices = [state["AskPrice"][node] for node in neighbors_with_availability]
+    print("Ask prices of direct neighbors with bonds available --> ", ask_prices)
+    # Number of nodes
+    num_neighbors = len(neighbors_with_availability)
+    # if all other askprices are zero, we can buy from any neighboring pools, if all other ask_prices are more than the starting nodes ask price, return starting node
+    if all(state["AskPrice"][node] == 0 or (state["AskPrice"][starting_node] < state["AskPrice"][node] and state["Availability"][starting_node] != 0) for node in neighbors_with_availability):
+        print("im here")
+        return previous_node
+    elif (len(neighbors_with_availability) == 0):
+        #return to the node before starting node
+        print("im here toooo")
+        return starting_node
+    else:
+        # Initialize the solution vector
+        x0 = np.zeros(num_neighbors)
 
-def transition(current_pool, visited, available_pools, G, state, action, orderAmount, order_exhausted, S, totalcost, originalorderAmount):
-    visited.add(current_pool)
+        # Initialize the objective function coefficients
+        c = [-price for price in ask_prices]
+
+        # Bounds for the variables (ask prices must be greater than 0)
+        bounds = [(0, None) for _ in range(num_neighbors)]
+
+        # Objective function to minimize
+        def objective_function(x):
+            return np.dot(c, x)
+
+        # Constraint: each node can be visited at most once
+        constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}
+
+        # Solve the linear programming problem
+        result = minimize(objective_function, x0, bounds=bounds, constraints=constraints)
+
+        # Extract the index of the optimal node
+        optimal_node_index = np.argmax(result.x)
+    previous_node = starting_node
+    # Return the optimal node
+    return neighbors_with_availability[optimal_node_index]
+
+def transition(pool_to_traverse, visited, G, state, action, orderAmount, order_exhausted, S, totalcost, originalorderAmount):
+    visited.add(pool_to_traverse)
     
     if sum(state["Availability"].values()) == 0:
-        exhausted_result = exhaustedState(current_pool, G, S, state, action, orderAmount, totalcost)
+        exhausted_result = exhaustedState(pool_to_traverse, G, S, state, action, orderAmount, totalcost)
         print(exhausted_result)
         order_exhausted[0] = True
     else:
-        neighbors = G.get(current_pool, [])  # Directly access neighbors from the graph
-
-        # Find available neighbors and sort them by ask prices
-        available_neighbors = [pool for pool in neighbors
-                            if pool in state["Availability"] and state["Availability"][pool] != 0]
-        available_neighbors.sort(key=lambda pool: state["AskPrice"][pool])
-
-        min_pool = None
-        min_ask_price = float('inf')  # Initialize with positive infinity
-        for pool in available_neighbors:
-            if pool not in visited and state["AskPrice"][pool] < min_ask_price:
-                min_pool = pool
-                min_ask_price = state["AskPrice"][pool]
-
-        if min_pool is not None:
-            orderticket = state["Availability"][min_pool] * min_ask_price
-            if orderAmount < state["Availability"][min_pool]:
-                orderticket = orderAmount * min_ask_price
-                state["Availability"][min_pool] -= orderAmount
-                orderAmount = 0
-            else:
-                orderAmount -= state["Availability"][min_pool]
-                state["Availability"][min_pool] = 0
-            totalcost += orderticket
-
-        # Check if orderAmount is still greater than 0 before attempting to execute at any available pool
-        for pool in available_pools:
-            if pool not in visited:
-                orderticket_pool = state["Availability"][pool] * state["AskPrice"][pool]
-                if orderAmount < state["Availability"][pool]:
-                    orderticket_pool = orderAmount * state["AskPrice"][pool]
-                    state["Availability"][pool] -= orderAmount
-                    orderAmount = 0
-                else:
-                    orderAmount -= state["Availability"][pool]
-                    state["Availability"][pool] = 0
-                totalcost += orderticket_pool
-        
-        # Recursively call transition on the next available pool
-        for pool in available_neighbors:
-            if pool not in visited and not order_exhausted[0]:
-                transition(pool, visited, available_pools, totalcost, orderAmount,order_exhausted, state, action, G, originalorderAmount, S)
+        if orderAmount < state["Availability"][pool_to_traverse]:
+            orderticket = orderAmount * state["AskPrice"][pool_to_traverse]
+            state["Availability"][pool_to_traverse] -= orderAmount
+            orderAmount = 0
+        else:
+            orderticket = state["Availability"][pool_to_traverse] * state["AskPrice"][pool_to_traverse]
+            orderAmount -= state["Availability"][pool_to_traverse]
+            state["Availability"][pool_to_traverse] = 0
+        totalcost += orderticket
+    pool_traversed = pool_to_traverse
     
-    return totalcost, min_pool, orderticket, orderAmount
+    return totalcost, pool_traversed, orderticket, orderAmount
 
 def exhaustedState(starting_pool, G, S, state, action, orderAmount, totalcost):
     print(orderAmount)
